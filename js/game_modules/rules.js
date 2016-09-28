085cf5f781fb49a3604559cc40bdc11adea83d62
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-09-28 22:31:15 +0800

    add document to rules module

--- js/game_modules/rules.js
+++ js/game_modules/rules.js
@@ -1,8 +1,23 @@
-// TODO: Should it be a deep copy or shallow copy ??
+/* Definition of Map Object */
+
+/*
+  Function: set surrounding alive cell number with which the center cell could become alive or dead.
+  Arguments: lifeNumberRange -> surrounding alive cell number with which the center cell could become alive
+             deathNumberRange -> surrounding alive cell number with which the center cell could become dead
+*/
+
 function Rules(lifeNumRange, remainNumRange){
   this.lifeNumRange = lifeNumRange;
   this.remainNumRange = remainNumRange;
 }
+
+/*
+  Function: return surrounding alive cell number
+  Arguments: map -> the map object which stores the cellmap
+             rowOrder -> the order of the row in cellmap
+             colOrder -> the order of the col in cellmap
+  Return: lifeNum -> the surrounding alive cell number, an integer between [0..8]           
+*/
 
 Rules.prototype.getNeighborLifeNum = function(map, rowOrd, colOrd){
   // console.log('h5');
@@ -19,6 +34,13 @@ Rules.prototype.getNeighborLifeNum = function(map, rowOrd, colOrd){
   return lifeNum;
 }
 
+/*
+  Function: return the cell next state based on the surrounding currently alive cell number
+  Arguments: currentCellState -> true or false, related to death or life
+             neighborLifeNum -> the surrounding currently alive cell number, between [0..8]
+  Return: the cell next state[true or false]           
+*/
+
 Rules.prototype.judgeNextCellStateByLifeNum = function(currentCellState, neighborLifeNum){
   if (this.lifeNumRange.indexOf(neighborLifeNum) !== -1){
     return true;
@@ -29,12 +51,25 @@ Rules.prototype.judgeNextCellStateByLifeNum = function(currentCellState, neighbo
   return false;
 }
 
+/*
+  Function: return the next state of the cell which located in (rowOrd, colOrd)
+  Arguments: map -> the map object which stores the cellmap
+             rowOrder -> the order of the row in cellmap
+             colOrder -> the order of the col in cellmap
+  Return: the cell next state[true or false]           
+*/
+
 Rules.prototype.getNextCellState = function(map, rowOrd, colOrd){
   // console.log('h3');
   var neighborLifeNum = this.getNeighborLifeNum(map, rowOrd, colOrd);
   // console.log('h4');
   return this.judgeNextCellStateByLifeNum(map.cellMap[rowOrd][colOrd], neighborLifeNum);
 }
+
+/*
+  Function: generate the next map according to current map
+  Arguments: map -> the map object which stores the cellmap          
+*/
 
 Rules.prototype.generateNextCellMap = function(map){
   var _this = this;
