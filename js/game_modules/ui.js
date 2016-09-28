e68677d3f147720b22279c8746dcb11ec178ffb2
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-09-28 22:40:12 +0800

    add document to ui.js

--- js/game_modules/ui.js
+++ js/game_modules/ui.js
@@ -1,3 +1,13 @@
+/* Definition of UI Object */
+
+/*
+  Function: custruct a ui object with some settings 
+  Arguments: canvas -> the <canvas> document element
+             lineColor -> string that represents the line color when painting
+             liveColor -> string that represents the alive cell color when painting
+             deathColor -> string that represents the dead cell color when painting
+*/
+
 function UI(canvas, lineColor, liveColor, deathColor){
   this.canvas = canvas;
   this.ctx = canvas.getContext('2d');
@@ -6,7 +16,12 @@ function UI(canvas, lineColor, liveColor, deathColor){
   this.deathColor = deathColor;
 };
 
-// output the map structure
+/*
+  Function: drawing grid system on canvas 
+  Arguments: rows -> the row number of the grid system
+             cols -> the col number of the grid system
+*/
+
 UI.prototype.outputFrame = function(rows, cols){
   var cellWidth = this.canvas.width / cols,
       cellHeight = this.canvas.height / rows,
@@ -25,6 +40,15 @@ UI.prototype.outputFrame = function(rows, cols){
   }
 };
 
+/*
+  Function: drawing a cell whose ordinate is (rowOrder, colOrder) on canvas 
+  Arguments: rowOrder -> the row order of the ordinate
+             colOrder -> the col order of the ordinate
+             cellWidth -> the width of a cell in px
+             cellHeight -> the height of a cell in px
+             alive -> true or false, represents the live or death
+*/
+
 UI.prototype.outputCell = function(rowOrder, colOrder, cellWidth, cellHeight, alive){
   if (alive){
     this.ctx.fillStyle = this.liveColor;
@@ -34,6 +58,11 @@ UI.prototype.outputCell = function(rowOrder, colOrder, cellWidth, cellHeight, al
   
   this.ctx.fillRect(colOrder * cellWidth, rowOrder * cellHeight, cellWidth, cellHeight);
 }
+
+/*
+  Function: drawing a cell map on canvas 
+  Arguments: cellMap -> two-dimension array, representing the cell map
+*/
 
 UI.prototype.outputMap = function(cellMap){
   var _this = this,
