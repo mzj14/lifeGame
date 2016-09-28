6352df0598c373083fb770f74faf7fc28f510142
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-09-28 22:31:28 +0800

    add document to timer module.

--- js/game_modules/timer.js
+++ js/game_modules/timer.js
@@ -1,32 +1,46 @@
-// timerLength and timeUnit are in seconds.
+/* Definition of Timer Object */
+
+/*
+  Function: set game total time(in second) and cell evolution period(in second)
+  Arguments: timeLength -> game total time(in second)
+             timeUnit -> cell evolution period(in second)
+*/
+
 function Timer(timeLength, timeUnit){
   this.timeLength = Math.floor(timeLength / timeUnit);
   this.timeUnit = timeUnit;
   this.timerID = undefined;
 }
 
-// map is a Map object, rule is a Rule object
+/*
+  Function: run game with the help of map, rules and ui
+  Arguments: map -> map object
+             rules -> rules object
+             ui -> ui object
+*/
+
 Timer.prototype.runGame = function(map, rules, ui){
   var _this = this;
   ui.outputMap(map.cellMap);
   ui.outputFrame(map.rows, map.cols);
-  console.log(this.timeLength);
   var passTime = 0;
   this.timerID = setInterval(function(){
     passTime++; 
-    console.log(passTime);
     // Stop refresh the map if time is over
     if (passTime >= _this.timeLength){
       _this.stopGame();
     }
     else{
       rules.generateNextCellMap(map);
-      console.log(map.cellMap[0]);
       ui.outputMap(map.cellMap);
       ui.outputFrame(map.rows, map.cols);
     }
   }, this.timeUnit * 1000);
 }
+
+/* 
+  Function: clear timer to stop game;
+*/  
 
 Timer.prototype.stopGame = function(){
   clearInterval(this.timerID);
